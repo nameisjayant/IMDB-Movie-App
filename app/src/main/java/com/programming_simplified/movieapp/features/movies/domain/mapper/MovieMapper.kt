ec7638f963be6eb6878ec993cae835cd6ace1c89
nameisjayant <jayntkumar99@gmail.com>
2022-08-23 23:28:14 +0530

    mapped with model file

--- app/src/main/java/com/programming_simplified/movieapp/features/movies/domain/mapper/MovieMapper.kt
+++ app/src/main/java/com/programming_simplified/movieapp/features/movies/domain/mapper/MovieMapper.kt
@@ -2,18 +2,21 @@ package com.programming_simplified.movieapp.features.movies.domain.mapper
 
 import com.programming_simplified.movieapp.common.base.Mapper
 import com.programming_simplified.movieapp.data.model.Movies
+import com.programming_simplified.movieapp.features.movies.domain.model.MovieUiModel
 import javax.inject.Inject
 
-class MovieMapper @Inject constructor() : Mapper<Movies?, List<Movies.Results>?> {
+class MovieMapper @Inject constructor() : Mapper<Movies?, List<MovieUiModel>?> {
 
-    override fun mapFrom(from: Movies?): List<Movies.Results>? {
+    override fun mapFrom(from: Movies?): List<MovieUiModel>? {
         return from?.results?.map {
-            Movies.Results(
-                id = it?.id,
-                original_title = it?.original_title,
-                overview = it?.overview,
-                poster_path = it?.poster_path,
-                vote_average = it?.vote_average
+            MovieUiModel(
+               Movies.Results(
+                   id = it?.id,
+                   original_title = it?.original_title,
+                   overview = it?.overview,
+                   poster_path = it?.poster_path,
+                   vote_average = it?.vote_average
+               )
             )
         }
     }
--- app/src/main/java/com/programming_simplified/movieapp/features/movies/domain/model/MovieUiModel.kt
+++ app/src/main/java/com/programming_simplified/movieapp/features/movies/domain/model/MovieUiModel.kt
@@ -3,5 +3,5 @@ package com.programming_simplified.movieapp.features.movies.domain.model
 import com.programming_simplified.movieapp.data.model.Movies
 
 data class MovieUiModel(
-    val results:List<Movies.Results?>?
+    val results:Movies.Results
 )
